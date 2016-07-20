265bf27a9f6d31dcdecd263bc7b1e6f00e2cd473
mmockelyn <sysdev@cridip.com>
2016-07-20 13:54:50 +0200

    PAYPAL IPN

--- view/template.php
+++ view/template.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -22,7 +22,7 @@
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="#">
-                <img src="<?= $redirect->getUrl(array('images', 'logo/')); ?>logo.png" class="img-responsive">
+                <img src="<?= $redirect->getUrl(array('images', 'logo/')); ?>logo.png" class="img-responsive" style="position: relative; top:-12px;">
             </a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
