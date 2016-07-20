bba5221d5fdfd12065a72746f00080e8ae387e64
mmockelyn <sysdev@cridip.com>
2016-07-20 13:45:08 +0200

    PAYPAL IPN

--- view/template.php
+++ view/template.php
@@ -8,9 +8,34 @@
     <title><?= $constante->NOM_SITE; ?></title>
     <link rel="stylesheet" href="<?= $redirect->getUrl(array('css/')); ?>bootstrap.css">
     <link rel="stylesheet" href="<?= $redirect->getUrl(array('css/')); ?>bootstrap-theme.css">
+    <link rel="stylesheet" href="<?= $redirect->getUrl(array('css/')); ?>starter-template.css">
 </head>
 <body>
-<h1>Hello, world!</h1>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+<div class="container">
+
+</div>
+
 <script src="https://code.jquery.com/jquery-3.1.0.js" integrity="sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk=" crossorigin="anonymous"></script>
 <script src="<?= $redirect->getUrl(array('js/')); ?>bootstrap.js"></script>
 <script src="<?= $redirect->getUrl(array('js/')); ?>paypal.js"></script>
